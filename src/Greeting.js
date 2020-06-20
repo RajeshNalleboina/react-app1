abdbc9632ce33cfb32f3591503661e7179c998d8
RajeshNalleboina <rajeshnalleboina94@gmail.com>
2020-06-20 16:33:15 +0530

    reactjs

---
+++ src/Greeting.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Welcome from './Welcome'
+
+let Greeting=()=>{
+    return(
+        <div>
+            <h1>Greeting</h1>
+            <Welcome name="Rajesh"></Welcome>
+            <Welcome name="Ravindra"></Welcome>
+            <Welcome name="Kalyan Garu"></Welcome>
+            <Welcome name=" Hanu"></Welcome>
+
+
+        </div>
+    );
+
+}
+
+export default Greeting;
